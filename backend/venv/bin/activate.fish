445789737bbab15f8d854792ac3776de94db85e5
jaydenfont <jaydenfont@gmail.com>
2022-04-01 11:37:19 -0400

    initializes requirements.txt

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jaydenfont/Desktop/Code/School/cs411-project/venv"
+set -gx VIRTUAL_ENV "/Users/jaydenfont/Desktop/Code/School/cs411-project/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
