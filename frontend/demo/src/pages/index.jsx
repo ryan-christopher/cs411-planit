70a9006ac9095a7edc9b1f3b1130a0b49c6015d8
ryan-christopher <rechristopher99@gmail.com>
2022-04-10 23:48:26 -0400

    landing page and team page edits

--- frontend/demo/src/pages/index.jsx
+++ frontend/demo/src/pages/index.jsx
@@ -2,20 +2,65 @@ import React from 'react'
 import Preloader from '../components/Preloader'
 import Navbar from '../components/Navbar'
 import "../style/pages.css"
+import "../style/home.css"
 
 function Home() {
+
     return (
         <div>
             <Preloader />
             <Navbar />
             <div className="header-section" id="landing">
                 <div id="landingmask">
-                    <h1>Planit</h1>
-                    <div id="carousel">
-                        {/*
-                        <img src="pictures/icon-bar-drinks.png" alt="YOYOYOYO" />
-    */}
+                    <h1>PLANIT</h1>
 
+
+                    <img src="pictures/planit-logo.png" alt="The planit logo." id="landingpic" />
+
+
+                    <div id="carousel">
+                        {/* FIRST HALF BANNER */}
+                        <img id="carousel-pic-1" src="pictures/icon-bar-drinks.png" alt="bars" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-2" src="pictures/icon-coffee.png" alt="coffee" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-3" src="pictures/icon-concert.png" alt="concerts" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-4" src="pictures/icon-food-2.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-5" src="pictures/icon-food.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-1" src="pictures/icon-bar-drinks.png" alt="bars" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-2" src="pictures/icon-coffee.png" alt="coffee" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-3" src="pictures/icon-concert.png" alt="concerts" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-4" src="pictures/icon-food-2.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-5" src="pictures/icon-food.png" alt="food" />
+                        <div className="spacer"></div>
+                        {/* SECOND HALF BANNER */}
+                        <img id="carousel-pic-1" src="pictures/icon-bar-drinks.png" alt="bars" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-2" src="pictures/icon-coffee.png" alt="coffee" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-3" src="pictures/icon-concert.png" alt="concerts" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-4" src="pictures/icon-food-2.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-5" src="pictures/icon-food.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-1" src="pictures/icon-bar-drinks.png" alt="bars" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-2" src="pictures/icon-coffee.png" alt="coffee" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-3" src="pictures/icon-concert.png" alt="concerts" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-4" src="pictures/icon-food-2.png" alt="food" />
+                        <div className="spacer"></div>
+                        <img id="carousel-pic-5" src="pictures/icon-food.png" alt="food" />
+                        <div className="spacer"></div>
                     </div>
                 </div>
             </div>
--- frontend/demo/src/pages/team.jsx
+++ frontend/demo/src/pages/team.jsx
@@ -8,9 +8,94 @@ function Team() {
         <div>
             <Preloader />
             <Navbar />
-            <br />
-            Team!
+            <div className="header-section" id="team">
+                <div id="landingmask">
+                    <h1>Our team</h1>
+                    <h3>meet the makers</h3>
+                    <div id="content">
+                        {/* 
+                    Abdul Ka, Jayden Font, Kevin Martin, 
+                    Lauren Pearson, Rachel Ferrigno, 
+                    Ryan Christopher
+                    */}
+                        <div className="flex-container-left-right">
+                            <div className="flex-item-left">
+                                <h2>Abdul</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>hey</p>
+                            </div>
+                        </div>
 
+                        <hr className="line" />
+
+                        <div className="flex-container-right-left">
+                            <div className="flex-item-left">
+                                <h2>Jayden</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>yo</p>
+                            </div>
+                        </div>
+
+                        <hr className="line" />
+
+                        <div className="flex-container-left-right">
+                            <div className="flex-item-left">
+                                <h2>Kevin</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>hey</p>
+                            </div>
+                        </div>
+
+                        <hr className="line" />
+
+                        <div className="flex-container-right-left">
+                            <div className="flex-item-left">
+                                <h2>Lauren</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>yo</p>
+                            </div>
+                        </div>
+
+                        <hr className="line" />
+
+                        <div className="flex-container-left-right">
+                            <div className="flex-item-left">
+                                <h2>Rachel</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>hey</p>
+                            </div>
+                        </div>
+
+                        <hr className="line" />
+
+                        <div className="flex-container-right-left">
+                            <div className="flex-item-left">
+                                <h2>Ryan</h2>
+                                <img src="pictures/cat.png" alt="Abdul." className="pfp" />
+                            </div>
+                            <div className="flex-item-right">
+                                <p>yo</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <img src="pictures/planit-logo.png" alt="The planit logo." id="landingpic" />
+                </div>
+            </div>
         </div>
 
     )
