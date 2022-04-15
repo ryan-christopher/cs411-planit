8191f8127fe7f79b8144dd688052c28ffd5a27b5
jaydenfont <jaydenfont@gmail.com>
2022-04-15 17:00:12 -0400

    adds integration to navigation API

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, json, jsonify, request
 import requests  # used to make external API calls
 from flask_cors import CORS
+import os
+import dotenv
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
+dotenv.load_dotenv()
 
 @app.route("/")
 def landing():
-    return {"response": "Success!"}, 200
+    return {"response": "Success"}, 200
 
 @app.route("/get_trains_by_line", methods=['POST'])
 def get_line():
@@ -22,9 +25,8 @@ def get_line():
         if "-" in req['line']:
             req['line'] = req['line'][:-1] + req['line'][-1].capitalize()
 
-    print(req)
     res = requests.get(f"https://api-v3.mbta.com/routes/{req['line']}").json()
-    print(res)
+
     # if invalid line, return error from V3 API with applicable status code
     if "errors" in res:
         status_code = int(res['errors'][0]['status'])
@@ -32,6 +34,24 @@ def get_line():
         
     # returns valid input
     return {"response": res}, 200
+
+@app.route('/get_directions_between_coords', methods=['POST'])
+def get_directions_between_coords():
+    """Given start and end coordinates, return the set of directions along with other route details"""
+    req = request.get_json()
+    try:
+        depart_lat = str(req['depart_lat'])
+        depart_lon = str(req['depart_lon'])
+        dest_lat = str(req['dest_lat'])
+        dest_lon = str(req['dest_lon'])
+    except KeyError:
+        return {"response": "Invalid Coordinates"}, 400
+    if not depart_lat or not depart_lon or not dest_lat or not dest_lon:
+        return {"response": "Invalid Coordinates"}, 400
+    api_key = os.getenv("GRAPHHOPPERKEY")
+    url = f"https://graphhopper.com/api/1/route?point={depart_lat}, {depart_lon}&point={dest_lat}, {dest_lon}&profile=car&locale=en&instructions=true&calc_points=true&key={api_key}"
+    res = requests.get(url)
+    return res.text, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
