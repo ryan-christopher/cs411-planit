e865bc94e574374c78a1ba1b488b36773464c1ca
ryan-christopher <rechristopher99@gmail.com>
2022-04-30 22:30:58 -0400

    senduserinfo added to homepage button

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -129,11 +129,27 @@ function Signup() {
 
     const [authenticated, setAuthenticated] = useState(false)
 
+    // gets user's info from google for db
+    const sendUserInfo = () => {
+        axios({
+            method: "GET",
+            url: "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + localStorage.getItem("accessToken")
+        })
+            .then((response) => {
+                axios({
+                    method: "POST",
+                    url: "http://127.0.0.1:5000/register_google_data",
+                    data: response['data']
+                })
+            })
+    }
+
     const responseGoogle = (response) => {
         console.log("logged into google")
         if ("accessToken" in response) {
             localStorage.setItem("accessToken", response['accessToken'])  // store in local storage, not ideal
             window.location.reload();
+            sendUserInfo();
         }
     }
 
